0e26dcef07ea079736f4609011191e234f62ef62
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2025-04-04 00:02:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let input1 = prompt("Enter the first number:");
-let input2 = prompt("Enter the second number:");
-
 // Write your code here and print the output using alert function
+
+let input1 = prompt("Enter the first number");
+ let input2 = prompt("Enter the second number");
+function sumTwo(){
+	let num1= parseFloat(input1);
+		let num2 = parseFloat(input2);
+	if(isNan(num1) || isNan(num2)){
+	  window.alert("Invalid input! Please enter valid numbers.");
+   return;
+	}
+	let sum = num1 + num2
+	console.log(`The sum of ${input1} and ${input2} is ${sum}`);
+    window.alert(`The sum of ${num1} and ${num2} is ${sum}`);
+
+} 
+sumTwo()
